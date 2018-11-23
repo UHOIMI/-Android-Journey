baf8bd751a496092b3fecf83a5f618024ab90c07
G015C1140\G015C1140 <horizon12.kuro@gmail.com>
2018-11-23 21:38:33 +0900

    変更

--- app/src/main/java/com/example/g015c1140/journey/Setting.kt
+++ app/src/main/java/com/example/g015c1140/journey/Setting.kt
@@ -2,16 +2,19 @@ package com.example.g015c1140.journey
 
 class Setting {
 
-    val IP_ADDRESS = ""
     val SERVER_ADDRESS = ""
+    val IP_ADDRESS = ""
+
+    val SERVER_IMAGE_POST_URL = "http://$SERVER_ADDRESS:443/api/v1/image/upload"
+
 
     val USER_LOGIN_URL = "http://$IP_ADDRESS/api/v1/users/login"
     val USER_GET_URL = "http://$IP_ADDRESS/api/v1/users/find"
     val USER_ACCOUNT_GET_URL = "http://$IP_ADDRESS/api/v1/users/find?user_id="
     val USER_POST_URL = "http://$IP_ADDRESS/api/v1/users/register"
-    val USER_GET_IMAGE_URL = "http://35.200.26.70:8080/test1/"
+    val USER_GET_IMAGE_URL = "http://$SERVER_ADDRESS:8080/test1/"
     val USER_PUT_URL = "http://$IP_ADDRESS/api/v1/users/update"
-    val SPOT_IMAGE_POST_URL = "http://$IP_ADDRESS/api/v1/image/upload"
+    val IMAGE_POST_URL = "http://$IP_ADDRESS/api/v1/image/upload"
     val SPOT_POST_URL = "http://$IP_ADDRESS/api/v1/spot/register/"
     val SPOT_GET_URL = "http://$IP_ADDRESS/api/v1/spot/find?user_id="
     val PLAN_POST_URL = "http://$IP_ADDRESS/api/v1/plan/register/"
