bced7c3c689b8f1f1f330152bb9de1e1d99bc9b7
Fujimori0220 <G016C1102@outlook.jp>
2018-11-29 11:35:49 +0900

    検索画面UI作成

--- app/src/main/java/com/example/g015c1140/journey/SearchPlanActivity.kt
+++ app/src/main/java/com/example/g015c1140/journey/SearchPlanActivity.kt
@@ -1,11 +1,22 @@
 package com.example.g015c1140.journey
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.view.View
+import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_search_plan.*
 
 class SearchPlanActivity : AppCompatActivity() {
+
+
+    //交通手段ボタン用
+    private lateinit var transportationImageButton: MutableList<ImageButton>
+
+    //交通手段ボタンフラグ用
+    private val TRANSPORTATION_IMAGE_FLG = mutableListOf(0, 0, 0, 0, 0, 0, 0)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +58,86 @@ class SearchPlanActivity : AppCompatActivity() {
             }
         }
         false
+    }
+
+
+    @SuppressLint("PrivateResource")
+    fun onTransportationButtonTapped(view: View) {
+
+        when (view.id) {
+            R.id.walkImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[0] = if (TRANSPORTATION_IMAGE_FLG[0] == 1) {
+                    transportationImageButton[0].setImageResource(R.drawable.s_walk_off)
+                    0
+                } else {
+                    transportationImageButton[0].setImageResource(R.drawable.s_walk_on)
+                    1
+                }
+            }
+
+            R.id.bicycleImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[1] = if (TRANSPORTATION_IMAGE_FLG[1] == 1) {
+                    transportationImageButton[1].setImageResource(R.drawable.s_bicycle_off)
+                    0
+                } else {
+                    transportationImageButton[1].setImageResource(R.drawable.s_bicycle_on)
+                    1
+                }
+            }
+
+            R.id.carImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[2] = if (TRANSPORTATION_IMAGE_FLG[2] == 1) {
+                    transportationImageButton[2].setImageResource(R.drawable.s_car_off)
+                    0
+                } else {
+                    transportationImageButton[2].setImageResource(R.drawable.s_car_on)
+                    1
+                }
+
+            }
+
+            R.id.busImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[3] = if (TRANSPORTATION_IMAGE_FLG[3] == 1) {
+                    transportationImageButton[3].setImageResource(R.drawable.s_bus_off)
+                    0
+                } else {
+                    transportationImageButton[3].setImageResource(R.drawable.s_bus_on)
+                    1
+                }
+
+            }
+
+            R.id.trainImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[4] = if (TRANSPORTATION_IMAGE_FLG[4] == 1) {
+                    transportationImageButton[4].setImageResource(R.drawable.s_train_off)
+                    0
+                } else {
+                    transportationImageButton[4].setImageResource(R.drawable.s_train_on)
+                    1
+                }
+            }
+
+            R.id.airplaneImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[5] = if (TRANSPORTATION_IMAGE_FLG[5] == 1) {
+                    transportationImageButton[5].setImageResource(R.drawable.s_airplane_off)
+                    0
+                } else {
+                    transportationImageButton[5].setImageResource(R.drawable.s_airplane_on)
+                    1
+                }
+            }
+
+            R.id.boatImageButton -> {
+                TRANSPORTATION_IMAGE_FLG[6] = if (TRANSPORTATION_IMAGE_FLG[6] == 1) {
+                    transportationImageButton[6].setImageResource(R.drawable.s_boat_off)
+                    0
+                } else {
+                    transportationImageButton[6].setImageResource(R.drawable.s_boat_on)
+                    1
+                }
+
+            }
+        }
     }
 
 
