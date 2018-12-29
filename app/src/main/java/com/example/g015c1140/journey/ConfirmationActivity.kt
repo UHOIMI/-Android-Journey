d6a293e5b4901aec5a8b6a9c8cf040155e67de28
G015C1140\G015C1140 <horizon12.kuro@gmail.com>
2018-12-29 19:06:59 +0900

    確認

--- app/src/main/java/com/example/g015c1140/journey/ConfirmationActivity.kt
+++ app/src/main/java/com/example/g015c1140/journey/ConfirmationActivity.kt
@@ -130,7 +130,6 @@ class ConfirmationActivity : AppCompatActivity() {
                         setPositiveButton("確認", null)
                         show()
                     }
-
                 }
             }
         })
